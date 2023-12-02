9895e784873ce5bed7b0bee492a4f4f4cd15ea5d
Mikael Lind <elemel@elemel.se>
2023-12-02 07:41:06 +0100

    Fix formatting

--- python/2023/12/01.py
+++ python/2023/12/01.py
@@ -1,36 +1,35 @@
 import sys
 
-
 DIGIT_NAMES = "one two three four five six seven eight nine".split()
 
 
 def parse_digits(line):
-	for i in range(len(line)):
-		if line[i].isdigit():
-			yield line[i]
+    for i in range(len(line)):
+        if line[i].isdigit():
+            yield line[i]
 
-		for j, name in enumerate(DIGIT_NAMES):
-			if line[i:].startswith(name):
-				yield str(j + 1)
+        for j, name in enumerate(DIGIT_NAMES):
+            if line[i:].startswith(name):
+                yield str(j + 1)
 
 
 def main():
-	lines = list(sys.stdin)
-	total = 0
+    lines = list(sys.stdin)
+    total = 0
 
-	for line in lines:
-		digits = [c for c in line if c.isdigit()]
-		total += int(digits[0] + digits[-1])
+    for line in lines:
+        digits = [c for c in line if c.isdigit()]
+        total += int(digits[0] + digits[-1])
 
-	print(total)
-	total = 0
+    print(total)
+    total = 0
 
-	for line in lines:
-		digits = list(parse_digits(line))
-		total += int(digits[0] + digits[-1])
+    for line in lines:
+        digits = list(parse_digits(line))
+        total += int(digits[0] + digits[-1])
 
-	print(total)
+    print(total)
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- python/2023/12/02.py
+++ python/2023/12/02.py
@@ -5,47 +5,47 @@ CUBES = dict(red=12, green=13, blue=14)
 
 
 def parse_handful(s):
-	handful = {}
+    handful = {}
 
-	for count_color_str in s.split(","):
-		count_str, color = count_color_str.split()
-		handful[color] = int(count_str)
+    for count_color_str in s.split(","):
+        count_str, color = count_color_str.split()
+        handful[color] = int(count_str)
 
-	return handful
+    return handful
 
 
 def parse_game(line):
-	id_str, handfuls_str = line.split(":")
-	_, id_str = id_str.split()
-	id_ = int(id_str)
+    id_str, handfuls_str = line.split(":")
+    _, id_str = id_str.split()
+    id_ = int(id_str)
 
-	handfuls = [parse_handful(s) for s in handfuls_str.split(";")]
-	return id_, handfuls
+    handfuls = [parse_handful(s) for s in handfuls_str.split(";")]
+    return id_, handfuls
 
 
 def is_possible_handful(handful):
-	return all(count <= CUBES[color] for color, count in handful.items())
+    return all(count <= CUBES[color] for color, count in handful.items())
 
 
 def is_possible_game(handfuls):
-	return all(is_possible_handful(h) for h in handfuls)
+    return all(is_possible_handful(h) for h in handfuls)
 
 
 def get_power(game):
-	max_counts = {}
+    max_counts = {}
 
-	for handful in game:
-		for color, count in handful.items():
-			max_counts[color] = max(count, max_counts.get(color, 0))
+    for handful in game:
+        for color, count in handful.items():
+            max_counts[color] = max(count, max_counts.get(color, 0))
 
-	return prod(max_counts.values())
+    return prod(max_counts.values())
 
 
 def main():
-	games = [parse_game(line) for line in sys.stdin]
-	print(sum(id_ for id_, handfuls in games if is_possible_game(handfuls)))
-	print(sum(get_power(game) for id_, game in games))
+    games = [parse_game(line) for line in sys.stdin]
+    print(sum(id_ for id_, handfuls in games if is_possible_game(handfuls)))
+    print(sum(get_power(game) for id_, game in games))
 
 
 if __name__ == "__main__":
-	main()
+    main()
