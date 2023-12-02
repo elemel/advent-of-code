4d9b7ad09cc52285c687dc885f3b26f9a90cb995
Mikael Lind <elemel@elemel.se>
2023-12-02 07:48:28 +0100

    Parse digits as ints

--- python/2023/12/01.py
+++ python/2023/12/01.py
@@ -6,19 +6,19 @@ DIGIT_NAMES = "one two three four five six seven eight nine".split()
 def parse_digits(line):
     for i in range(len(line)):
         if line[i].isdigit():
-            yield line[i]
+            yield int(line[i])
 
         for j, name in enumerate(DIGIT_NAMES):
             if line[i:].startswith(name):
-                yield str(j + 1)
+                yield j + 1
 
 
 def solve_part_1(lines):
     total = 0
 
     for line in lines:
-        digits = [c for c in line if c.isdigit()]
-        total += int(digits[0] + digits[-1])
+        digits = [int(c) for c in line if c.isdigit()]
+        total += 10 * digits[0] + digits[-1]
 
     return total
 
@@ -28,7 +28,7 @@ def solve_part_2(lines):
 
     for line in lines:
         digits = list(parse_digits(line))
-        total += int(digits[0] + digits[-1])
+        total += 10 * digits[0] + digits[-1]
 
     return total
 
