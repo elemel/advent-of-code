5a4785cd4ee9772ee6b32ffa73d210668cbac220
Mikael Lind <elemel@elemel.se>
2023-12-06 08:56:24 +0100

    Fix formatting

--- python/2023/12/06.py
+++ python/2023/12/06.py
@@ -3,32 +3,32 @@ from math import prod
 
 
 def get_distance(bt, t):
-	return bt * (t - bt)
+    return bt * (t - bt)
 
 
 def solve(t, d):
-	result = 0
+    result = 0
 
-	for bt in range(1, t):
-		if get_distance(bt, t) > d:
-			result += 1
+    for bt in range(1, t):
+        if get_distance(bt, t) > d:
+            result += 1
 
-	return result
+    return result
 
 
 def main():
-	times_str, distances_str = stdin.read().strip().splitlines()
+    times_str, distances_str = stdin.read().strip().splitlines()
 
-	times = [int(w) for w in times_str.split()[1:]]
-	distances = [int(w) for w in distances_str.split()[1:]]
+    times = [int(w) for w in times_str.split()[1:]]
+    distances = [int(w) for w in distances_str.split()[1:]]
 
-	print(prod(solve(t, d) for t, d in zip(times, distances)))
+    print(prod(solve(t, d) for t, d in zip(times, distances)))
 
-	t = int(''.join(times_str.split()[1:]))
-	d = int(''.join(distances_str.split()[1:]))
+    t = int("".join(times_str.split()[1:]))
+    d = int("".join(distances_str.split()[1:]))
 
-	print(solve(t, d))
+    print(solve(t, d))
 
 
 if __name__ == "__main__":
-	main()
+    main()
