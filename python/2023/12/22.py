15c2f56da3a0ea643fbaacc24e0507110e554f51
Mikael Lind <elemel@elemel.se>
2023-12-22 09:42:38 +0100

    Cleanup

--- python/2023/12/22.py
+++ python/2023/12/22.py
@@ -12,7 +12,7 @@ def parse_brick(s):
     return a, b
 
 
-def generate_horizontal_brick_positions(brick):
+def generate_horizontal_positions(brick):
     a, b = brick
 
     ax, ay, az = a
@@ -30,37 +30,37 @@ def add(a, b):
     return ax + bx, ay + by, az + bz
 
 
-def move_brick(brick, offset):
+def move(brick, offset):
     a, b = brick
     return add(a, offset), add(b, offset)
 
 
-def get_brick_min_z(brick):
+def get_min_z(brick):
     return brick[0][2]
 
 
-def get_brick_max_z(brick):
+def get_max_z(brick):
     return brick[1][2]
 
 
-def fall_bricks(bricks):
+def fall(bricks):
     new_bricks = []
     ground = defaultdict(int)
     fall_count = 0
 
     for brick in bricks:
-        horizontal_positions = list(generate_horizontal_brick_positions(brick))
+        horizontal_positions = list(generate_horizontal_positions(brick))
         ground_max_z = max(ground[p] for p in horizontal_positions)
-        fall_distance = get_brick_min_z(brick) - ground_max_z - 1
+        fall_distance = get_min_z(brick) - ground_max_z - 1
 
         if fall_distance:
-            brick = move_brick(brick, (0, 0, -fall_distance))
+            brick = move(brick, (0, 0, -fall_distance))
             fall_count += 1
 
         new_bricks.append(brick)
 
         for position in horizontal_positions:
-            ground[position] = get_brick_max_z(brick)
+            ground[position] = get_max_z(brick)
 
     return new_bricks, fall_count
 
@@ -73,11 +73,13 @@ def is_safe_brick(bricks, i):
 def main():
     bricks = [parse_brick(l.strip()) for l in stdin]
 
-    bricks.sort(key=get_brick_min_z)
-    bricks, _ = fall_bricks(bricks)
+    bricks.sort(key=get_min_z)
+    bricks, _ = fall(bricks)
 
-    print(sum(is_safe_brick(bricks, i) for i in range(len(bricks))))
-    print(sum(fall_bricks(bricks[:i] + bricks[i + 1 :])[1] for i in range(len(bricks))))
+    fall_counts = [fall(bricks[:i] + bricks[i + 1 :])[1] for i in range(len(bricks))]
+
+    print(sum(c == 0 for c in fall_counts))
+    print(sum(fall_counts))
 
 
 if __name__ == "__main__":
