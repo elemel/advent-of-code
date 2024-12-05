c95f995c8d2294cb33b5f9d28cc25de7ac07a9ae
Mikael Lind <elemel@elemel.se>
2024-12-05 09:38:15 +0100

    Solve 2024-12-05

---
+++ python/2024/12/05.py
@@ -0,0 +1,34 @@
+from sys import stdin
+from functools import cmp_to_key
+
+
+def parse_rule(s):
+    a, b = map(int, s.split("|"))
+    return a, b
+
+
+def parse_update(s):
+    return list(map(int, s.split(",")))
+
+
+def main():
+    rules_str, updates_str = stdin.read().split("\n\n")
+    rules = set(map(parse_rule, rules_str.splitlines()))
+    updates = list(map(parse_update, updates_str.splitlines()))
+
+    def cmp(a, b):
+        return int((b, a) in rules) - int((a, b) in rules)
+
+    answers = [0, 0]
+
+    for update in updates:
+        fixed_update = sorted(update, key=cmp_to_key(cmp))
+        incorrect = fixed_update != update
+        answers[incorrect] += fixed_update[len(fixed_update) // 2]
+
+    for answer in answers:
+        print(answer)
+
+
+if __name__ == "__main__":
+    main()
