ccf434bd3a2fa6b42caea74ec56f8e05a131d00a
Mikael Lind <elemel@elemel.se>
2024-12-05 11:26:48 +0100

    Revert inlining

--- python/2024/12/05.py
+++ python/2024/12/05.py
@@ -2,11 +2,20 @@ from sys import stdin
 from functools import cmp_to_key
 
 
+def parse_rule(line):
+    a, b = map(int, line.split("|"))
+    return a, b
+
+
+def parse_update(line):
+    return list(map(int, line.split(",")))
+
+
 def main():
     rules_str, updates_str = stdin.read().split("\n\n")
 
-    rules = {tuple(map(int, line.split("|"))) for line in rules_str.splitlines()}
-    updates = [list(map(int, line.split(","))) for line in updates_str.splitlines()]
+    rules = set(map(parse_rule, rules_str.splitlines()))
+    updates = list(map(parse_update, updates_str.splitlines()))
 
     def cmp(a, b):
         return int((b, a) in rules) - int((a, b) in rules)
